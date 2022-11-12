dd939a1face6648ae83bc35d57646486a9092aee
smil3fRomCZ <jan.melicherik84@gmail.com>
2022-11-12 16:35:26 +0100

    Created DB setup and migrations

---
+++ migrations/20221112152030-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      isActive: {
+        defaultValue: true,
+        type: Sequelize.BOOLEAN,
+      },
+      isActivated: {
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      registrationHash: {
+        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
